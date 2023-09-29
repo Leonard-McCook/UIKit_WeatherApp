426f962a3c1bb05c833960a33e0a2b770726480f
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-09-29 15:55:40 -0400

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Leonard McCook-Carr on 9/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
