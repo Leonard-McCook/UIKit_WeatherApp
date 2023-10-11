bb9b269ae8dcae07df9bc569cc6f0fe369c745f5
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-10-11 12:08:58 -0400

    Update CurrentWeatherView

--- WeatherApp/Managers/WeatherManager.swift
+++ WeatherApp/Managers/WeatherManager.swift
@@ -15,6 +15,10 @@ final class WeatherManager {
     
     let service = WeatherService.shared
     
+    public private(set) var currentWeather: CurrentWeather?
+    public private(set) var hourlyWeather: [HourWeather] = []
+    public private(set) var dailyWeather: [DayWeather] = []
+    
     private init() {}
     
     public func getWeather(for location: CLLocation, completion: @escaping () -> Void) {
@@ -25,6 +29,10 @@ final class WeatherManager {
                 print("Current: \(result.currentWeather)")
                 print("Hourly: \(result.hourlyForecast)")
                 print("Daily: \(result.dailyForecast)")
+                
+                self.currentWeather = result.currentWeather
+                self.dailyWeather = result.dailyForecast.forecast
+                self.hourlyWeather = result.hourlyForecast.forecast
                 
                 completion()
             } catch {
---
+++ WeatherApp/ViewModels/CurrentWeatherSection.swift
@@ -0,0 +1,14 @@
+//
+//  CurrentWeatherSection.swift
+//  WeatherApp
+//
+//  Created by Leonard McCook-Carr on 10/10/23.
+//
+
+import Foundation
+
+enum CurrentWeatherSection {
+    case current
+    case hourly
+    case daily
+}
