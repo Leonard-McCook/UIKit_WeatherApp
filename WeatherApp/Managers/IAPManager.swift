bbbd8341b52611052bf4434fe113a05739576a5d
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-10-18 09:31:19 -0400

    Import import RevenueCat in the IAPManager file

--- WeatherApp/Managers/IAPManager.swift
+++ WeatherApp/Managers/IAPManager.swift
@@ -4,11 +4,19 @@
 //
 //  Created by Leonard McCook-Carr on 9/30/23.
 //
-// TODO: Bring in RevenueCat
+
+import RevenueCat
 import Foundation
 
 final class IAPManager {
     static let shared = IAPManager()
-    
-    private init() {}                                               
+
+    private init() {}
+
+    func isSubscribed(completion: @escaping (Bool) -> Void) {
+        Purchases.shared.getCustomerInfo { info, error in
+            guard let subscriptions = info?.activeSubscriptions else { return }
+            completion(!subscriptions.isEmpty)
+        }
+    }
 }
